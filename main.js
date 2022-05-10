9dbf74643dcc5f63a1b93c60e93cf2659b893e2e
Alexander Fonseca <motoleco@hotmail.com>
2022-05-10 15:32:16 -0300

    site final

--- main.js
+++ main.js
@@ -2,100 +2,77 @@ window.addEventListener('scroll', onScroll)
 
 onScroll()
 function onScroll() {
-
   showNavOnScroll()
   showBackToTopButtonOnScroll()
 
-  activeMenuAtCurrentSection(home)
-  activeMenuAtCurrentSection(services)
-  activeMenuAtCurrentSection(about)
-  activeMenuAtCurrentSection(contact)
+  activateMenuAtCurrentSection(home)
+  activateMenuAtCurrentSection(services)
+  activateMenuAtCurrentSection(about)
+  activateMenuAtCurrentSection(contact)
 }
 
-function activeMenuAtCurrentSection(section){
-//linha alvo
-const targetLine = scrollY + innerHeight / 2
+function activateMenuAtCurrentSection(section) {
+  const targetLine = scrollY + innerHeight / 2
 
-// verificar se a seção passou da linha
-// quais dados vou precisar?
+  // verificar se a seção passou da linha
+  // quais dados vou precisar?
+  const sectionTop = section.offsetTop
+  const sectionHeight = section.offsetHeight
+  const sectionTopReachOrPassedTargetline = targetLine >= sectionTop
 
-//O topo da seção
-const sectionTop = section.offsetTop
+  // verificar se a base está abaixo da linha alvo
 
-//a altura da seção
-const sectionHeight = section.offsetHeight
+  const sectionEndsAt = sectionTop + sectionHeight
+  const sectionEndPassedTargetline = sectionEndsAt <= targetLine
 
-// o topo da seção chegou ou ultrapassou a linha alvo
-const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop
+  // limites da seção
+  const sectionBoundaries =
+    sectionTopReachOrPassedTargetline && !sectionEndPassedTargetline
 
-//verificar se a base está abaixo da linha alvo
-//quais dados vou precisar?
+  const sectionId = section.getAttribute('id')
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
 
-// a seção termina onde?
-const sectionEndsAt = sectionTop + sectionHeight
-
-// o final da seção passou a linha alvo
-const sectionEndPassedTargetLine = sectionEndsAt <= targetLine
-
-//limites da seção
-const sectionBoundaries = 
-sectionTopReachOrPassedTargetLine &&  !sectionEndPassedTargetLine 
-
-const sectionId = section.getAttribute('id')
-const menuElement = document.querySelector(`.menu a [href*=${sectionId}]`)
-
-menuElement.classList.remove('active')
-if (sectionBoundaries){
-  menuElement.classList.add('active')
+  menuElement.classList.remove('active')
+  if (sectionBoundaries) {
+    menuElement.classList.add('active')
+  }
 }
 
-}
-
-
-
-
-
-
-
-
-
-
-
-function showNavOnScroll(){
-  if(scrollY > 0){
+function showNavOnScroll() {
+  if (scrollY > 0) {
     navigation.classList.add('scroll')
-  }else{
+  } else {
     navigation.classList.remove('scroll')
   }
 }
 
-function showBackToTopButtonOnScroll(){
-  if(scrollY > 500){
+function showBackToTopButtonOnScroll() {
+  if (scrollY > 550) {
     backToTopButton.classList.add('show')
-  }else{
+  } else {
     backToTopButton.classList.remove('show')
   }
 }
 
-function openMenu(){
+function openMenu() {
   document.body.classList.add('menu-expanded')
 }
 
-function closeMenu(){
+function closeMenu() {
   document.body.classList.remove('menu-expanded')
 }
 
 ScrollReveal({
   origin: 'top',
   distance: '30px',
-  duration: 700,
-}).reveal(`#home, 
-#home img, 
-#home .stats, 
-#services,
-#services header,
-#services .card,
-#about,
-#about header,
-#about .content`)
-
+  duration: 700
+}).reveal(`
+  #home, 
+  #home img, 
+  #home .stats, 
+  #services,
+  #services header,
+  #services .card
+  #about, 
+  #about header, 
+  #about .content`)
